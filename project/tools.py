82eff0966e3707f5d432b6542010783dd407438a
ydlover <ydlover@gmail.com>
2018-01-27 00:17:27 +0800

    bug fix

--- project/tools.py
+++ project/tools.py
@@ -4,8 +4,10 @@ Created on 2018-1-26
 @author: ydlov
 '''
 
+import RPi.GPIO as GPIO  
+
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location  
     GPIO.cleanup()  
-        
+    print("clear success!")    
     
